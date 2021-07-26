c2ad9d065f7f6eeca06888301ef730bcf727566d
Simone de santis <simone.agentelazio@yahoo.it>
2021-07-26 15:35:34 +0200

    integrated search form

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+Ciao Ragazzi,
+Esercizio di oggi:
+cartella/repo js-es6-icons
+Milestone 1
+Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
+Milestone 2
+Coloriamo le icone per tipo
+Milestone 3
+Creiamo una select con i tipi di icone e usiamola per filtrare le icone
+:avviso:IMPORTANTE: non è obbligatorio usare la sintassi ES6, se non vi sentite a vostro agio partite dalla sintassi con cui vi trovate meglio e cercate di migliorarla in un secondo momento.
+BONUS: Provare ad aggiungere una casella di testo che renda possibile filtrare anche per nome dell'icona
+SUPER INCREDIBLE MEGA BONUS: effettuare la ricerca dalla casella di testo in "tempo reale", cioè man mano che l'utente digita.
+s */
